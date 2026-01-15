027b57ba485759f7a9cd2fed44c595400043cb85
r2hu1 <newasram86@gmail.com>
2026-01-15 13:21:32 +0530

    dn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Crimson_Text } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/lib/constants";
+import { Analytics } from "@vercel/analytics/next";
 
 const _geist = Geist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://rahul.eu.org"),
-  title: siteConfig.name,
+  title: `r2hu1 (${siteConfig.name})`,
   description: siteConfig.description,
   keywords: [
     "fullstack engineer",
@@ -43,6 +44,7 @@ export default function RootLayout({
       <body className={`${_geist.className} antialiased`}>
         <main className="px-4 py-5">{children}</main>
         <div className="inset-0 -mt-16 h-full w-full items-center px-5 py-20 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+        <Analytics />
       </body>
     </html>
   );
