ed53b0428715f75d4e719e9270a082022fa221dc
r2hu1 <newasram86@gmail.com>
2026-03-23 00:14:28 +0530

    feat: dn

--- components/hero.tsx
+++ components/hero.tsx
@@ -12,22 +12,13 @@ export function Hero() {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, delay: 0.3 }}
-      className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 py-16 pt-4"
+      className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 py-16 pt-0 -mt-5"
     >
       <div className="space-y-2">
         <h1 className="text-muted-foreground">
-          Hi, I'm {siteConfig.name}, a self-taught developer who genuinely
-          enjoys building things.
+          Hi, I'm {siteConfig.name}, a fullstack engineer based in india. You
+          can explore my works, or reach out to me anytime.
         </h1>
-        <p className="text-muted-foreground">
-          I focus on creating products that are reliable, scale when they need
-          to, and look clean without trying too hard. Full-stack development is
-          where I’m most comfortable: clear APIs, solid backend foundations, and
-          interfaces that feel smooth rather than flashy.
-        </p>
-        <p className="text-muted-foreground">
-          You can explore my works, or reach out to me anytime.
-        </p>
         <div className="mt-5 flex items-center gap-2">
           <Button
             size="sm"
--- components/working.tsx
+++ components/working.tsx
@@ -6,21 +6,23 @@ const data = [
   {
     name: "Open-Source Developer",
     position: "Github",
-    description:
-      "I also love contributing to open source! I’ve built a bunch of open-source projects myself from web apps, apis, micro apps to backend systems and even some AI stuff. Plus, I’ve contributed to several other open-source projects along the way",
+    description: `I also love contributing to open source! I've built a bunch of open-source projects myself from
+      web apps, apis, micro apps to backend systems and even some Al stuff. Plus, I've contributed
+      to several other open-source projects along the way.`,
     start_date: "2022",
   },
   {
     name: "Tayst AI",
     position: "Founding Engineer",
-    description:
-      "As a Founding Engineer at Tayst, a U.S.-based startup, I’m building the company’s scalable backend and microservices infrastructure, while also contributing to the frontend to deliver a seamless user experience.",
+    description: `As a Founding Engineer at Tayst, a U.S.-based startup, I'm building the company's scalable
+    backend and microservices infrastructure, while also contributing to the frontend to deliver a
+    seamless user experience.`,
     start_date: "17/09/2025",
     end_date: "17/11/2025",
     promotion: {
       title: "Tech Lead",
-      description:
-        "I'm responsible for leading the development of the company's backend, frontend and microservices infrastructure, ensuring scalability and reliability.",
+      description: `I'm responsible for leading the development of the company's backend, frontend and
+      microservices infrastructure, ensuring scalability and reliability.`,
       start_date: "17/11/2025",
       end_date: "15/03/2026",
     },
@@ -28,8 +30,10 @@ const data = [
   {
     name: "Portals",
     position: "Frontend Developer",
-    description:
-      "It was a (UK) London based startup with great vision and a strong team of developers. I worked there as frontend developer, building their web application including the main landing page. And worked in integrating their backend apis with frontend.",
+    description: `Built and maintained all dynamic pages for the startup's web platform using Next.js, React,
+    TypeScript, and Tailwind CSS. Focused on creating responsive, high-performance Uls and
+    integrating APIs to deliver a smooth user experience. Collaborated directly with the founding
+    team on product features and frontend architecture.`,
     start_date: "04/03/25",
     end_date: "03/06/25",
   },
