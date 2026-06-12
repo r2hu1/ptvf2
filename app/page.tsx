bcd4c9a18780dccc01414f96913473a9377988e7
r2hu1 <newasram86@gmail.com>
2026-06-12 13:08:18 +0530

    Wrap page content with LayoutGroup and add Preloader

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
+"use client";
+
+import { LayoutGroup } from "framer-motion";
 import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
 import { Projects } from "@/components/projects";
+import { Preloader } from "@/components/preloader";
 import WorkingExperience from "@/components/working";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black py-8 sm:py-16 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-2xl mx-auto">
-        <Header />
-        <Hero />
-        <WorkingExperience />
-        <Projects />
-      </div>
-    </main>
+    <LayoutGroup>
+      <Preloader />
+      <main className="min-h-screen bg-black py-8 sm:py-16 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-2xl mx-auto">
+          <Header />
+          <Hero />
+          <WorkingExperience />
+          <Projects />
+        </div>
+      </main>
+    </LayoutGroup>
   );
 }
