96666b9bcef86246a792bd1548b262748824fc00
r2hu1 <newasram86@gmail.com>
2026-06-20 12:57:28 +0530

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { siteConfig } from "@/lib/constants";
 import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "@/components/ui/sonner";
 import { PreloaderProvider } from "@/components/preloader-context";
+import Head from "next/head";
 
 const _geist = Geist({ subsets: ["latin"] });
 
@@ -49,6 +50,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+      <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+        ></script>
+      </Head>
       <body
         className={`${_geist.className}
         antialiased`}
