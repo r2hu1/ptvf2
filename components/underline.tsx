1f51c2efe071f8676c8f473ab549382d1cd9857c
r2hu1 <newasram86@gmail.com>
2026-08-03 20:48:18 +0530

    feat: add HoverText component with hover-card popover

--- components/underline.tsx
+++ components/underline.tsx
@@ -1,7 +1,28 @@
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
+
 export function Underline({ children }: { children: React.ReactNode }) {
   return (
     <span className="underline underline-offset-3 text-white/80">
       {children}
     </span>
+  );
+}
+
+export function HoverText({
+  text,
+  children,
+}: {
+  text: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <HoverCard>
+      <HoverCardTrigger>
+        <span className="underline underline-offset-3 text-white/80">
+          {text}
+        </span>
+      </HoverCardTrigger>
+      <HoverCardContent>{children}</HoverCardContent>
+    </HoverCard>
   );
 }
